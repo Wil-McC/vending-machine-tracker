201d2a421014e22628298396d040773e84238fb3
Wil McCauley <wmccauley724@gmail.com>
2021-02-21 14:04:24 -0700

    Finish stories with model and feature testing

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -4,4 +4,18 @@ class Machine < ApplicationRecord
   belongs_to :owner
   has_many :snack_machines
   has_many :snacks, through: :snack_machines
+
+  def price_avg
+    price_total / snack_count
+  end
+
+  def price_total
+    snacks.sum do |snack|
+      snack.price
+    end
+  end
+
+  def snack_count
+    snacks.count
+  end
 end
